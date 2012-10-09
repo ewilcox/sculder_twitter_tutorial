710189fb356ae7bd013f1439fb7da1bf6f7a3d54
Eric Wilcox <ewilcox42@gmail.com>
2012-10-08 22:13:53 -0600

    Got camera working from his base tutorial
    Small image is stored locally and loaded, this opens the camera to take
    the photo.

--- assets/www/js/app.js
+++ assets/www/js/app.js
@@ -1,11 +1,24 @@
- function onLoad(){
+var pictureSource,
+	destinationType;
+
+function onLoad(){
       document.addEventListener("deviceready", onDeviceReady, true);
  }
+function getPhoto(imageData) {
+	var smallImage = document.getElementById('smallImage');
+    smallImage.style.display = 'block';
+    smallImage.src = imageData;
+}
+function capturePhoto() {
+    navigator.camera.getPicture(getPhoto, onError, { quality: 50 });
+}
  function onDeviceReady(){
     //alert("PhoneGap is working!!");
+	 pictureSource=navigator.camera.PictureSourceType;
+     destinationType=navigator.camera.DestinationType;
  }
- function onError(){
-     alert("Error");
+ function onError(message){
+     alert("Error: " + message);
  }
 // iscroll stuff - taking out now for jQuery mobile instead - works much better
 // var theScroll;
@@ -15,19 +28,19 @@
 // document.addEventListener('DOMContentLoaded', scroll, false);
  
  function getTweets(data) {
-	 console.log(data);
-	var output='<ul data-role="listview" data-theme="a">';
-	$.each(data.results, function(key, val) {
-		//console.log(val.text);
-		var text=val.text,
-			thumbnail= val.profile_image_url,
+	 //console.log(data);
+	 var output='<ul data-role="listview" data-theme="a">';
+	 $.each(data.results, function(key, val) {
+		 //console.log(val.text);
+		 var text=val.text,
+		 	thumbnail= val.profile_image_url,
 			name=val.from_user_name,
 			user=val.from_user;
-	    output+= '<li>'
-		output += '<img src="'+thumbnail +'" alt = "Photo of "'+name +'">';
-		output += '<div data-role="controlgroup"><a href="http://twitter.com/'+user+'"><h2>'+name+'</h2></a>'+text+'</div>';
-		output +='</li>';
-	});
-	output += '</ul>';
-	$('#tweetlist').html(output);
+		 output+= '<li>';
+		 output += '<img src="'+thumbnail +'" alt = "Photo of "'+name +'">';
+		 output += '<div data-role="controlgroup"><a href="http://twitter.com/'+user+'"><h2>'+name+'</h2></a>'+text+'</div>';
+		 output +='</li>';
+	 });
+	 output += '</ul>';
+	 $('#tweetlist').html(output);
 }
